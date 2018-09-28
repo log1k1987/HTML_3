0c9eeb0e9426e46c199c38cdffd3a7d7e5000141
log1k <log1k@mail.ru>
2018-09-28 18:47:28 +0300

    parallax index.html

--- src/assets/scripts/auth.js
+++ src/assets/scripts/auth.js
@@ -1,1 +1,17 @@
-console.log("auth module loaded");
+const parallaxContainer = document.getElementById('parallax');
+const layers = parallaxContainer.children;
+
+const moveLayers = e => {
+    const initialX = window.innerWidth / 2 - e.pageX;
+    const initialY = window.innerHeight / 2 - e.pageY;
+
+    Array.from(layers).forEach((layer, i) => {
+        const divider = i / 100;
+        const positionX = initialX * divider;
+        const positionY = initialY * divider;
+
+        layer.style.transform = `translate(${positionX}px, ${positionY}px)`;
+    });
+};
+
+window.addEventListener('mousemove', moveLayers);
